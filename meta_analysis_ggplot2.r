3d9276dabf36b7f96fec9610c0b1c8f5b7058208
anascacais <anascacais@gmail.com>
2024-02-27 12:00:15 +0100

    corrected misalignment between forest plot and annotations

--- meta_analysis_ggplot2.r
+++ meta_analysis_ggplot2.r
@@ -44,10 +44,9 @@ dat <- mutate(dat, key = paste(
 dat$key <- as.character(dat$key)
 
 ### order data according to subgroup and yi
-dat <- dat[order(dat$subgroup, dat$Year), decreasing = FALSE]
-subgroups <- sort(unique(dat$subgroup))
+dat <- dat[order(dat$Year, decreasing = TRUE), ]
+subgroups <- sort(unique(dat$subgroup), decreasing = TRUE)
 subgroup_counts <- table(dat$subgroup)
-subgroup_counts_dict <- as.list(names = names(subgroup_counts), subgroup_counts)
 
 res_dict <- setNames(lapply(subgroups, function(x) list(count = subgroup_counts[[x]])), subgroups)
 dat <- transform(dat, ci_low = yi - 1.96 * sqrt(vi), ci_upp = yi + 1.96 * sqrt(vi))
@@ -60,12 +59,10 @@ dat <- mutate(dat, weights = weights)
 get_summary <- function(dat, y) {
     ### fit random-effects model
     res <- rma(dat$yi, dat$vi, data = dat)
-    ci_midpoint <- (res$ci.ub - res$ci.lb) / 2 + res$ci.lb
-
 
     summary_polygon <- data.frame(
-        x = c(res$ci.ub, ci_midpoint, res$ci.lb, ci_midpoint),
-        y = c(y - 1, y - 1 + 0.5, y - 1, y - 1 - 0.5)
+        x = c(res$ci.ub, coef(res), res$ci.lb, coef(res)),
+        y = c(y, y + 0.25, y, y - 0.25)
     )
 
     return(list(summary_polygon, res))
@@ -75,7 +72,7 @@ get_summary <- function(dat, y) {
 # get forest plot
 
 layers <- list()
-y_iter <- 1
+y_iter <- 2
 
 entries <- data.frame(
     entry = numeric(),
@@ -85,19 +82,31 @@ entries <- data.frame(
     stringsAsFactors = FALSE
 )
 
-for (sg in names(res_dict)) {
+# Entry for titles (last entry) + empty entry after each subgroup summary
+n_entries <- length((dat$yi)) + length(subgroups) * 2 + 2
+
+for (sg in rev(names(res_dict))) {
     # get subset of data for this subgroup and set the y-axis value as a sequence
     dat_subgroup <- subset(dat, subgroup == sg)
-    dat_subgroup <- mutate(dat_subgroup, yn = seq(y_iter, y_iter + res_dict[[sg]]$count - 1))
+    dat_subgroup <- mutate(dat_subgroup, yn = seq(y_iter + 1, y_iter + res_dict[[sg]]$count))
 
     # get summary for this subgroup
     output <- get_summary(dat_subgroup, y_iter)
     summary_polygon <- output[[1]]
     res_aux <- output[[2]]
 
+    # add subgroup label
+    new_entries <- data.frame(
+        entry = n_entries - y_iter - res_dict[[sg]]$count,
+        label = sg,
+        weight = "",
+        metric = ""
+    )
+    entries <- rbind(entries, new_entries)
+
     # create label between the entry and the Study and respective weight
     new_entries <- data.frame(
-        entry = dat_subgroup$yn,
+        entry = seq(n_entries - y_iter, n_entries - y_iter - res_dict[[sg]]$count + 1, by = -1),
         label = paste(dat_subgroup$Authors, dat_subgroup$Year, sep = ", "),
         weight = dat_subgroup$weights,
         metric = paste(fmtx(dat_subgroup$yi, digits = 2), " (", fmtx(dat_subgroup$ci_low, digits = 2), "-", fmtx(dat_subgroup$ci_upp, digits = 2), ")", sep = "")
@@ -105,36 +114,64 @@ for (sg in names(res_dict)) {
     entries <- rbind(entries, new_entries)
 
     new_entries <- data.frame(
-        entry = c(y_iter + res_dict[[sg]]$count, y_iter + res_dict[[sg]]$count + 1),
-        label = c(
+        entry = n_entries - y_iter + 1,
+        label =
             paste(
-                "Subtotal", sg, "(I^2 =",
+                "Subtotal (I^2 =",
                 round(res_aux$I2, digits = 3), "%, ",
                 fmtp(res_aux$QMp, digits = 2, pname = "p", add0 = TRUE, sep = TRUE, equal = TRUE),
                 ")"
-            ), ""
-        ),
-        weight = c(sum(as.double(dat_subgroup$weights)), ""),
-        metric = c("", "")
+            ),
+        weight = sum(as.double(dat_subgroup$weights)),
+        metric = paste(fmtx(coef(res_aux), digits = 2), " (", fmtx(res_aux$ci.lb, digits = 2), "-", fmtx(res_aux$ci.ub, digits = 2), ")", sep = "")
     )
     entries <- rbind(entries, new_entries)
 
     # Create layers for this subgroup, adding the mean metric, CI and summary polygon
+
     subgroup_layers <- list(
-        geom_point(
-            data = dat_subgroup, aes(x = yi, y = yn), shape = 15, size = 3
-        ),
-        geom_linerange(data = dat_subgroup, aes(xmin = ci_low, xmax = ci_upp, y = yn)),
         geom_polygon(
             data = summary_polygon, aes(x = x, y = y),
             fill = "gray", alpha = 0.3, inherit.aes = FALSE
-        )
+        ),
+        geom_point(
+            data = dat_subgroup, aes(x = yi, y = yn), shape = 15, size = 3
+        ),
+        geom_linerange(data = dat_subgroup, aes(xmin = ci_low, xmax = ci_upp, y = yn))
     )
 
     layers <- c(layers, subgroup_layers)
+
     # add empty entry between subgroups
     y_iter <- y_iter + res_dict[[sg]]$count + 2
 }
+
+
+# Add summary for all studies
+output <- get_summary(dat, 1)
+summary_polygon <- output[[1]]
+res <- output[[2]]
+summary_layer <- list(
+    geom_polygon(
+        data = summary_polygon, aes(x = x, y = y),
+        fill = "gray", alpha = 0.3, inherit.aes = FALSE
+    )
+)
+layers <- c(layers, summary_layer)
+
+new_entries <- data.frame(
+    entry = n_entries,
+    label = paste(
+        "Overall (I^2 =",
+        round(res$I2, digits = 3), "%, ",
+        fmtp(res$QMp, digits = 2, pname = "p", add0 = TRUE, sep = TRUE, equal = TRUE),
+        ")"
+    ),
+    weight = "100.00%",
+    metric = paste(fmtx(coef(res), digits = 2), " (", fmtx(res$ci.lb, digits = 2), "-", fmtx(res$ci.ub, digits = 2), ")", sep = "")
+)
+entries <- rbind(new_entries, entries)
+# entries[entries$entry == n_entries, ] <- new_entries
 
 
 # Combine all layers into a single plot
@@ -157,13 +194,11 @@ forest_plot <- forest_plot +
         axis.ticks.y = element_blank(),
         axis.text.y = element_blank(),
         axis.title.y = element_blank()
-    )
-plot(forest_plot)
+    ) +
+    coord_cartesian(ylim = c(0.5, n_entries))
 
-############################################################################
-# get y-axis for the forest plot
+# plot(forest_plot)
 
-entries$entry <- entries$entry + 1
 new_entry <- data.frame(
     entry = 1,
     label = "Study",
@@ -171,6 +206,11 @@ new_entry <- data.frame(
     metric = "AUC (95% CI)"
 )
 entries <- rbind(new_entry, entries)
+entries <- entries[order(entries$entry, decreasing = FALSE), ]
+
+
+############################################################################
+# get y-axis for the forest plot
 
 p_labels <-
     entries |>
@@ -181,20 +221,21 @@ p_labels <-
     p_labels +
     geom_text(aes(x = 0, label = label),
         hjust = 0,
-        fontface = ifelse(grepl("Subtotal", entries$label) | grepl("Study", entries$label) | grepl("Overall", entries$label), "bold", "plain")
+        fontface = ifelse(grepl("Subtotal", entries$label) | grepl("Study", entries$label) | grepl("Overall", entries$label) | (entries$label %in% subgroups), "bold", "plain")
     )
 
 # remove the background and edit the sizing so that this left size of the plot will match up neatly with the middle and right sides of the plot
 p_labels <-
     p_labels +
     theme_void() +
-    coord_cartesian(xlim = c(0, 4.5))
+    coord_cartesian(xlim = c(0, 4.5), ylim = c(0.5, n_entries))
 
 # plot(p_labels)
 
 
 ############################################################################
 # get extra annotations
+
 
 p_annot <- entries |>
     ggplot(aes(y = rev(entry)))
@@ -217,17 +258,17 @@ p_annot <- p_annot +
 p_annot <-
     p_annot +
     theme_void() +
-    coord_cartesian(xlim = c(0, 2.5))
+    coord_cartesian(xlim = c(0, 2), ylim = c(0.5, n_entries))
 
 # plot(p_annot)
 
 # ############################################################################
 
-# layout <- c(
-#     area(t = 0, l = 0, b = 30, r = 3), # left plot, starts at the top of the page (0) and goes 30 units down and 3 units to the right
-#     area(t = 1, l = 4, b = 30, r = 9), # middle plot starts a little lower (t=1) because there's no title. starts 1 unit right of the left plot (l=4, whereas left plot is r=3), goes to the bottom of the page (30 units), and 6 units further over from the left plot (r=9 whereas left plot is r=3)
-#     area(t = 0, l = 9, b = 30, r = 11) # right most plot starts at top of page, begins where middle plot ends (l=9, and middle plot is r=9), goes to bottom of page (b=30), and extends two units wide (r=11)
-# )
+layout <- c(
+    area(t = 0, l = 0, b = 30, r = 3), # left plot, starts at the top of the page (0) and goes 30 units down and 3 units to the right
+    area(t = 0, l = 4, b = 30, r = 9), # starts 1 unit right of the left plot (l=4, whereas left plot is r=3), goes to the bottom of the page (30 units), and 6 units further over from the left plot (r=9 whereas left plot is r=3)
+    area(t = 0, l = 10, b = 30, r = 12) # right most plot starts at top of page, begins where middle plot ends (l=9, and middle plot is r=9), goes to bottom of page (b=30), and extends two units wide (r=11)
+)
 
-p <- p_labels + forest_plot + p_annot # + plot_layout(design = layout)
-# plot(p)
+p <- p_labels + forest_plot + p_annot + plot_layout(design = layout)
+plot(p)
