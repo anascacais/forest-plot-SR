26ea76e8af86f195c8208c8499fe99f6f84c3791
anascacais <anascacais@gmail.com>
2024-03-18 14:30:54 +0000

    added two columns to label on the right (forecast horizon, train-test approach)

--- meta_analysis_ggplot_clocktime.r
+++ meta_analysis_ggplot_clocktime.r
@@ -93,6 +93,8 @@ entries <- data.frame(
     label = character(),
     weight = character(),
     metric = character(),
+    horizon = character(),
+    approach = character(),
     stringsAsFactors = FALSE
 )
 
@@ -114,7 +116,9 @@ for (sg in rev(names(res_dict))) {
         entry = n_entries - y_iter - res_dict[[sg]]$count + 0.5,
         label = sg,
         weight = "",
-        metric = ""
+        metric = "",
+        horizon = "",
+        approach = ""
     )
     entries <- rbind(entries, new_entries)
 
@@ -123,7 +127,9 @@ for (sg in rev(names(res_dict))) {
         entry = seq(n_entries - y_iter, n_entries - y_iter - res_dict[[sg]]$count + 1, by = -1),
         label = dat_subgroup$"First author, year",
         weight = dat_subgroup$weights,
-        metric = paste(fmtx(dat_subgroup$yi, digits = 2), " (", fmtx(dat_subgroup$ci_low, digits = 2), "-", fmtx(dat_subgroup$ci_upp, digits = 2), ")", sep = "")
+        metric = paste(fmtx(dat_subgroup$yi, digits = 2), " (", fmtx(dat_subgroup$ci_low, digits = 2), "-", fmtx(dat_subgroup$ci_upp, digits = 2), ")", sep = ""),
+        horizon = dat_subgroup$"Forecast horizon",
+        approach = ifelse(dat_subgroup$"Training and testing approach" == "prospective", "(*)", "")
     )
     entries <- rbind(entries, new_entries)
 
@@ -137,7 +143,9 @@ for (sg in rev(names(res_dict))) {
                 ")"
             ),
         weight = sum(as.double(dat_subgroup$weights)),
-        metric = paste(fmtx(coef(res_aux), digits = 2), " (", fmtx(res_aux$ci.lb, digits = 2), "-", fmtx(res_aux$ci.ub, digits = 2), ")", sep = "")
+        metric = paste(fmtx(coef(res_aux), digits = 2), " (", fmtx(res_aux$ci.lb, digits = 2), "-", fmtx(res_aux$ci.ub, digits = 2), ")", sep = ""),
+        horizon = "",
+        approach = ""
     )
     entries <- rbind(entries, new_entries)
 
@@ -188,11 +196,11 @@ new_entries <- data.frame(
         ")"
     ),
     weight = "100.00",
-    metric = paste(fmtx(coef(res), digits = 2), " (", fmtx(res$ci.lb, digits = 2), "-", fmtx(res$ci.ub, digits = 2), ")", sep = "")
+    metric = paste(fmtx(coef(res), digits = 2), " (", fmtx(res$ci.lb, digits = 2), "-", fmtx(res$ci.ub, digits = 2), ")", sep = ""),
+    horizon = "",
+    approach = ""
 )
 entries <- rbind(new_entries, entries)
-# entries[entries$entry == n_entries, ] <- new_entries
-
 
 # Combine all layers into a single plot
 forest_plot <- ggplot() +
@@ -201,20 +209,14 @@ forest_plot <- ggplot() +
 
 # rename axes
 forest_plot <- forest_plot +
-    labs(x = variable, y = "")
+    labs(x = variable, y = 0) # y=""
 
 
 # Add effect line
 line_data <- data.frame(x = c(coef(res), coef(res)), y = c(0, n_entries - 1 + 0.5))
 forest_plot <- forest_plot +
-    # geom_hline(yintercept = 1.5) +
-    # geom_hline(yintercept = 10.5) +
-    # geom_hline(yintercept = 17.5) +
     geom_hline(yintercept = n_entries - 1 + 0.5) +
     geom_line(data = line_data, aes(x = x, y = y), linetype = "dashed")
-
-
-
 
 forest_plot <- forest_plot +
     theme(
@@ -225,13 +227,16 @@ forest_plot <- forest_plot +
     ) +
     coord_cartesian(ylim = c(1, n_entries))
 
+
 # plot(forest_plot)
 
 new_entry <- data.frame(
     entry = 1,
     label = "Study",
     weight = "Weight (%)",
-    metric = paste(variable, "(95% CI)", sep = " ")
+    metric = paste(variable, "(95% CI)", sep = " "),
+    horizon = "Forecast Horizon",
+    approach = "(*)"
 )
 entries <- rbind(new_entry, entries)
 entries <- entries[order(entries$entry, decreasing = FALSE), ]
@@ -248,24 +253,34 @@ p_labels <- entries |>
 # add the study as text (instead of as a label on the y axis)
 p_labels <-
     p_labels +
-    # geom_hline(yintercept = 1.5) +
-    # geom_hline(yintercept = 10.5) +
-    # geom_hline(yintercept = 17.5) +
     geom_hline(yintercept = n_entries - 1 + 0.5) +
-    geom_text(aes(x = 0, label = label),
+    geom_text(
+        aes(x = 0, label = label),
         hjust = 0,
         fontface = ifelse(grepl("Subtotal", entries$label) | grepl("Study", entries$label) | grepl("Overall", entries$label) | (entries$label %in% subgroups), "bold", "plain"),
     )
 
-# p_labels <- ggplot(entries, aes(y = rev(entry))) +
-#     # geom_point() + # Add points (just as an example)
-#     geom_text(aes(x = 1, label = paste(label, entry)), y = rev(entries$entry), vjust = -0.5) # Add text annotations
+p_labels <- p_labels +
+    geom_text(
+        aes(x = 1.5, label = horizon),
+        hjust = 0.5,
+        fontface = ifelse(grepl("Forecast", entries$horizon), "bold", "plain"),
+    )
+
+p_labels <- p_labels +
+    geom_text(
+        aes(x = 2.5, label = approach),
+        hjust = 0.5,
+        fontface = ifelse(grepl("Forecast", entries$horizon), "bold", "plain"),
+    )
+
+
 
 # remove the background and edit the sizing so that this left size of the plot will match up neatly with the middle and right sides of the plot
 p_labels <-
     p_labels +
     theme_void() +
-    coord_cartesian(xlim = c(0, 4.5), ylim = c(1, n_entries))
+    coord_cartesian(xlim = c(0, 2.5), ylim = c(1, n_entries))
 
 plot(p_labels)
 
@@ -273,48 +288,44 @@ plot(p_labels)
 ############################################################################
 # get extra annotations
 
-
-# p_annot <- entries |>
-#     ggplot(aes(y = rev(entry)))
-
 p_annot <- entries |>
     ggplot(aes(y = seq(max(entry), min(entry), by = -1)))
 
 
 p_annot <- p_annot +
     geom_text(
-        aes(x = 0, label = weight),
-        hjust = 0,
+        aes(x = 0.5, label = weight),
+        hjust = 0.5,
         fontface = ifelse(entries$weight == "Weight (%)", "bold", "plain")
     )
 
 p_annot <- p_annot +
     geom_text(
-        aes(x = 1, label = metric),
-        hjust = 0,
+        aes(x = 1.5, label = metric),
+        hjust = 0.5,
         fontface = ifelse(grepl("(95% CI)", entries$metric), "bold", "plain")
     )
 
 # remove the background and edit the sizing so that this left size of the plot will match up neatly with the middle and right sides of the plot
 p_annot <-
     p_annot +
-    # geom_hline(yintercept = 1.5) +
-    # geom_hline(yintercept = 10.5) +
-    # geom_hline(yintercept = 17.5) +
     geom_hline(yintercept = n_entries - 1 + 0.5) +
     theme_void() +
     coord_cartesian(xlim = c(0, 2), ylim = c(1, n_entries))
 
-# plot(p_annot)
+plot(p_annot)
 
 # ############################################################################
 
 layout <- c(
     area(t = 0, l = 0, b = 30, r = 4), # left plot, starts at the top of the page (0) and goes 30 units down and 3 units to the right
     area(t = 0, l = 5, b = 30, r = 9), # starts 1 unit right of the left plot (l=4, whereas left plot is r=3), goes to the bottom of the page (30 units), and 6 units further over from the left plot (r=9 whereas left plot is r=3)
-    area(t = 0, l = 10, b = 30, r = 12) # right most plot starts at top of page, begins where middle plot ends (l=9, and middle plot is r=9), goes to bottom of page (b=30), and extends two units wide (r=11)
+    area(t = 0, l = 10, b = 30, r = 13) # right most plot starts at top of page, begins where middle plot ends (l=9, and middle plot is r=9), goes to bottom of page (b=30), and extends two units wide (r=11)
 )
+
 
 p <- p_labels + forest_plot + p_annot + plot_layout(design = layout)
 plot(p)
 ggsave(paste("forest_", paste(output_approach, variable, sep = "_"), ".pdf", sep = ""), plot = p)
+
+# TODO: make height of the plot dynamic (b = 30)
